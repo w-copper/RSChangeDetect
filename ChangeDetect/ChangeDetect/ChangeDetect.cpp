85925b9fcb5f1b2370c87024c57bb30c396dd150
WT <kingcu16@163.com>
2019-09-18 21:38:31 +0800

    compute element

--- ChangeDetect/ChangeDetect/ChangeDetect.cpp
+++ ChangeDetect/ChangeDetect/ChangeDetect.cpp
@@ -2,9 +2,10 @@
 //
 
 #include "stdafx.h"
-
+#include <math.h>
 #include "opencv2/core.hpp"
 #include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
 #include "gdal_priv.h"
 
 using namespace cv;
@@ -59,6 +60,13 @@ int main()
 			}
 		}
 	}
+	for (int i = 0; i < Bands; i++) {
+		delete[] pBand[i];
+		pBand[i] = nullptr;
+	};
+	delete[] pBand;
+	pBand = nullptr;
+	GDALClose(poDataset);
 	//get embi
 	const int DARR[11] = {0, 5, 10, 15, 20, 30, 45, 60, 70, 80, 90 };
 	const int SARR[4] = { 5, 10, 15, 20 };
@@ -71,6 +79,8 @@ int main()
 		}
 	}
 	embi /= 4 * 11 * 1.0f;
+
+	//thresold
 	Mat_<uchar> BinaryImg(Width, Height, (uchar)0);
 	float MinMax[2] = { embi(0,0), embi(0,0) };
 	for (int r = 0; r < Height; r++) {
@@ -84,10 +94,67 @@ int main()
 			BinaryImg(r, c) = uchar((embi(r, c) - MinMax[0]) / (MinMax[1] - MinMax[0]) * 255);
 		}
 	}
-	
-	//thresold
+	adaptiveThreshold(BinaryImg, BinaryImg, 255, ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY, 7, 1);
 
 	//post processing
+	//img save and show and relase
+	imwrite("../embi.tif", embi);
+	namedWindow("embi");
+	imshow("embi", embi);
+	embi.release();
+	imwrite("../binary.tif", BinaryImg);
+	namedWindow("binary");
+	imshow("binary", BinaryImg);
+	BinaryImg.release();
+
     return 0;
 }
 
+Mat &createStrictLineElement(int d, int s) {
+	assert(d >= 0 && d <= 45);
+	assert(s >= 2);
+	int width = int(s * cos(d * 1.0) + 0.5);
+	int height = int(s * sin(d*1.0) + 0.5 );
+	Mat_<uchar> e(width, height, (uchar)0);
+	double k = tan(d);
+	for (int i = 0; i < width; i++) {
+		int j = int(round(k * i));
+		if (j >= height) j = height;
+		e(i, j) = 1;
+	}
+	return e;
+}
+
+Mat &createLineElement(int d, int s) {
+	if (s <= 1) {
+		return Mat_<uchar>(1, 1, (uchar)1);
+	}
+	while (d >= 180) {
+		d -= 180;
+	}
+
+	if (d <= 45) {
+		return createStrictLineElement(d, s);
+	}
+	else if (d <= 90) {
+		Mat e = createStrictLineElement(d - 45, s).t();
+		return e;
+	}
+	else if (d <= 135) {
+		Mat e = createStrictLineElement(180 - d, s).t();
+		flip(e, e, 0);
+		return e;
+	}
+	else {
+		Mat e = createStrictLineElement(180 - d, s);
+		flip(e, e, 0);
+		return e;
+	}
+}
+
+Mat &computeWTH(Mat &src, int d, int s) {
+
+}
+Mat &computeDMBP(Mat &src, int d, int s, int ds) {
+
+}
